57ab6c8a63ad2cf6e534c6e0513495d28c86e5cf
Alyssa Hooper <nesteralyssa@gmail.com>
2020-03-30 14:50:29 -0300

    new routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { ListComponent } from './list/list.component';
+import { TestComponent } from './test/test.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'list', component: ListComponent },
+  { path: 'test', component: TestComponent },
 ];
 
 @NgModule({
